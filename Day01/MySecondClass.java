1927882de3864490c65579d0034c6b7b2f5db51e
솨 <l@naver.com>
2022-06-25 16:24:18 +0900

    커밋 테스트

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -1,4 +1,11 @@
 
-public class MySecondClass {
+public class MySecondClass { //자바 클래스 시작
+//클래스 블록
+	
+	  public static void main(String[] args) { //메소드 시작
+		  //매서드 블록, .... 문장들... 
+		  System.out.println("Git commit 후 변경해보기");
+		  
+	  } //메소드 끝
 
-}
+} //자바 클래스 끝
